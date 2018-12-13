423c041a6e83548113166edf1925f3fa2958e355
Anant Manjulkar <manjulkar_a@India.XoriantCorp.com>
2018-12-13 17:47:16 +0530

    merge test

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,10 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Lion always ROAR");
+		foo();
+	}
+	
+	private static void foo() {
+		System.out.println("Hey.....Good practice buddy.......");
 	}
 }
