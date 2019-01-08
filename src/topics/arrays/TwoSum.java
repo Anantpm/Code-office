5730dc3a6f2e65cd57d33de916e1a5fbce2bd711
Anant Manjulkar <manjulkar_a@India.XoriantCorp.com>
2019-01-08 11:06:37 +0530

    array coding inteview questions

---
+++ src/topics/arrays/TwoSum.java
@@ -0,0 +1,38 @@
+/*
+ * Given an array of Integers, return indices of the two numbers such that they add up to a specific target.
+ * 
+ * Input: {2,7,11,15}, Target : 9
+ * 
+ * Output: [0,1]  
+ */
+package topics.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] a = { 2, 7, 11, 15, 9, 4 };
+		int target = 11;
+		getPair(a, target);
+
+	}
+
+	private static void getPair(int[] a, int target) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int arr[] = new int[2];
+		for (int i = 0; i < a.length; i++) {
+			Integer val = map.get(target - a[i]);
+			if (val == null) {
+				map.put(a[i], i);
+			} else {
+				arr[0] = val;
+				arr[1] = i;
+			}
+		}
+//		System.out.println(map);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
