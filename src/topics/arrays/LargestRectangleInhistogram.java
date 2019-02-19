f85e76b96c3fb6be92db1de891390416edb9a83f
Anant Manjulkar <manjulkar_a@India.XoriantCorp.com>
2019-02-19 10:53:25 +0530

    Largest rectangular area in the histogram

---
+++ src/topics/arrays/LargestRectangleInhistogram.java
@@ -0,0 +1,52 @@
+package topics.arrays;
+
+import java.util.Stack;
+
+/**
+ * @author : Anant Manjulkar
+ *
+ *         Problem : Find the largest rectangular area possible in a given
+ *         histogram where the largest rectangle can be made of a number of
+ *         contiguous bars. For simplicity, assume that all bars have same width
+ *         and the width is 1 unit.
+ * 
+ *         Input : {6, 2, 5, 4, 5, 1, 6}
+ *
+ *         Output : 12
+ *
+ */
+public class LargestRectangleInhistogram {
+	public static void main(String[] args) {
+		int[] a = { 6, 2, 5, 4, 5, 1, 6 };
+		System.out.println("Maximum area is : " + getMaxRectangularArea(a));
+	}
+
+	private static int getMaxRectangularArea(int[] a) {
+		int area = 0;
+		Stack<Integer> stack = new Stack<>();
+		int areaTop;
+		int tp;
+		int i = 0;
+		while (i < a.length) {
+			if (stack.empty() || a[stack.peek()] <= a[i])
+				stack.push(i++);
+			else {
+				tp = stack.peek();
+				stack.pop();
+				areaTop = a[tp] * (stack.empty() ? i : i - stack.peek() - 1);
+				if (area <= areaTop) {
+					area = areaTop;
+				}
+			}
+		}
+		while (!stack.empty()) {
+			tp = stack.peek();
+			stack.pop();
+			areaTop = a[tp] * (stack.empty() ? i : i - stack.peek() - 1);
+			if (area <= areaTop) {
+				area = areaTop;
+			}
+		}
+		return area;
+	}
+}
