cef24c7a76628893df3b645da9ef0482c36a0351
Anant\Dev <anantj288@gmail.com>
2020-02-10 23:33:04 +0530

    Shuffle array without dups

--- src/topics/arrays/ShuffleArray.java
+++ src/topics/arrays/ShuffleArray.java
@@ -12,22 +12,20 @@ import java.util.Random;
  *         solution = new Solution(nums);
  * 
  *         // Shuffle the array [1,2,3] and return its result. Any permutation
- *         of [1,2,3] must equally likely to be returned. 
- *         solution.shuffle();
+ *         of [1,2,3] must equally likely to be returned. solution.shuffle();
  * 
  *         // Resets the array back to its original configuration [1,2,3].
  *         solution.reset();
  * 
- *         // Returns the random shuffling of array [1,2,3]. 
- *         solution.shuffle();
+ *         // Returns the random shuffling of array [1,2,3]. solution.shuffle();
  */
 
 public class ShuffleArray {
 
 	public static void main(String[] args) {
-		int[] a  = {1,2,3,4};
+		int[] a = { 1, 2, 3, 4 };
 		Solution s = new Solution(a);
-		
+
 		System.out.println(Arrays.toString(s.shuffle()));
 		System.out.println(Arrays.toString(s.reset()));
 		System.out.println(Arrays.toString(s.shuffle()));
@@ -36,28 +34,28 @@ public class ShuffleArray {
 
 }
 
-class Solution{
+class Solution {
 	private int[] nums;
 	private Random rand;
-	
-	Solution(int[] nums){
+
+	Solution(int[] nums) {
 		this.nums = nums;
 		rand = new Random();
 	}
-	
+
 	public int[] reset() {
 		return nums;
 	}
-	
+
 	public int[] shuffle() {
-		if(nums == null)
+		if (nums == null)
 			return null;
 		int[] a = nums.clone();
-		for(int i=0;i<a.length;i++) {
+		for (int i = 0; i < a.length; i++) {
 			int j = rand.nextInt(a.length);
-			swap(a,j,i);
+			swap(a, j, i);
 		}
-		
+
 		return a;
 	}
 
@@ -66,5 +64,5 @@ class Solution{
 		a[j] = a[i];
 		a[i] = temp;
 	}
-	
+
 }
